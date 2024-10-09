77010e2697039b61b11bf4934e5884e0aac1c1c2
Khushal <khushal@Khushals-iMac.local>
2024-10-09 11:57:30 +0530

    feat: implement typeorm and update the coffee service to use the postgresql database
    - write a docker-compose file to run postgresql in a docker container

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,26 +1,42 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll() {
-    return 'All Coffees.';
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
-    return `This is Coffee #${id}.`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
-  createCoffee(@Body() body: CreateCoffeeDto) {
-    return body;
+  create(@Body() body: CreateCoffeeDto) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
-  updateCoffee(@Param('id') id: number, @Body() body: UpdateCoffeeDto) {
-    return { id, ...body };
+  update(@Param('id') id: number, @Body() body: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.coffeesService.remove(id);
   }
 }
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+
+  findAll() {
+    return this.coffeeRepository.find();
+  }
+
+  async findOne(id: number) {
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id },
+    });
+
+    if (!coffee) {
+      // throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+      throw new NotFoundException('Coffee not found');
+    }
+
+    return coffee;
+  }
+
+  create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(coffee);
+  }
+
+  async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+    const coffee = await this.coffeeRepository.preload({
+      id: id,
+      ...updateCoffeeDto,
+    });
+
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+
+    return this.coffeeRepository.save(coffee);
+  }
+
+  async remove(id: number) {
+    const coffee = await this.findOne(id);
+    return this.coffeeRepository.remove(coffee);
+  }
+}
